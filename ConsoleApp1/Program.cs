0a83592eeb45bfec35b32efb93eeadb14ad15410
Zulfaabam <zulfafatahakbar@gmail.com>
2026-06-04 11:07:01 +0700

    print foobar

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,14 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Task: Print FooBar
 
-void PrintFishBash(int n)
+PrintFooBar(15);
+
+string PrintFooBar(int n)
 {
-    // string output = "";
+    List<string> output = new List<string> {};
 
-    for (int i = 0; i <= n; i++)
+    for (int i = 1; i <= n; i++)
     {
-        // string res = "";
-        if (n % 3 == 0) Console.WriteLine("Fish");
-        if (n % 5 == 0) Console.WriteLine("Bash");
+        string res = "";
+
+        if (i % 3 == 0) res += "foo";
+
+        if (i % 5 == 0) res += "bar";
+
+        if (i % 3 != 0 && i % 5 != 0) res += i.ToString();
+
+        output.Add(res);
+        res = "";
     }
+
+    string outputStr = string.Join(", ", output);
+    Console.WriteLine(outputStr);
+
+    return outputStr;
 }
