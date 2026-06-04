1590367eae176d7e5cb4f5a63fe7fe2d67a6b284
Zulfaabam <zulfafatahakbar@gmail.com>
2026-06-04 10:47:32 +0700

    intial commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+void PrintFishBash(int n)
+{
+    // string output = "";
+
+    for (int i = 0; i <= n; i++)
+    {
+        // string res = "";
+        if (n % 3 == 0) Console.WriteLine("Fish");
+        if (n % 5 == 0) Console.WriteLine("Bash");
+    }
+}
