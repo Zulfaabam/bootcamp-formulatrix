b760ecbea2fa1bf3582b079ba0a9f37e669f4cc7
Zulfaabam <zulfafatahakbar@gmail.com>
2026-06-08 14:54:52 +0700

    warung project

--- Warung/Program.cs
+++ Warung/Program.cs
@@ -5,20 +5,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Warung........");
+        Console.WriteLine("=============Warung============\n");
+
+        Console.WriteLine($"Stok Minyak Goreng: {new MinyakGoreng().JumlahStok}");
+        Console.WriteLine($"Stok Beras: {new Beras().JumlahStok}\n");
 
         Pembeli pembeli = new Pembeli("Abam");
-        Pembeli pembeli2 = new Pembeli("maba");
+        Pembeli pembeli2 = new("maba");
 
         pembeli.MembeliBarang(PilihanBarang.MinyakGoreng, 5);
+        pembeli.MembeliBarang(PilihanBarang.Beras, 3);
         pembeli2.MembeliBarang(PilihanBarang.Beras, 2);
 
+        Console.WriteLine("Ada yang beli....");
         Console.WriteLine($"Pembeli 1: {pembeli.Name}, Jumlah Membeli: {pembeli.BarangDibeli}");
         Console.WriteLine($"Pembeli 2: {pembeli2.Name}, Jumlah Membeli: {pembeli2.BarangDibeli}");
         Console.WriteLine($"\nTotal Pembeli: {Pembeli.TotalPembeli}");
-        Console.WriteLine($"\nTotal Barang Dibeli: {Pembeli.TotalBarangDibeli}");
+        Console.WriteLine($"Total Barang Dibeli: {Pembeli.TotalBarangDibeli}");
+        
         Console.WriteLine($"\nSisa Minyak Goreng: {new MinyakGoreng().JumlahStok}");
-        Console.WriteLine($"\nSisa Beras: {new Beras().JumlahStok}");
+        Console.WriteLine($"Sisa Beras: {new Beras().JumlahStok}");
+
+        Console.WriteLine("\n=============Warung============");
     }
 }
 
