04eefaa6c80464b05decfc9ae5fda3b8df60d646
Zulfaabam <zulfafatahakbar@gmail.com>
2026-06-15 13:16:40 +0700

    logic exercise 3

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,25 +1,37 @@
-﻿// Task: Print FooBar
+﻿// Logic Exercise
 
 PrintFooBar(15);
 PrintFooBar(21);
 PrintFooBar(35);
 PrintFooBar(105);
+PrintFooBar(63);
 
 string PrintFooBar(int n)
 {
     List<string> output = new List<string> {};
 
+    Dictionary<int, string> divisions = new()
+    {
+      {3, "foo"},
+      {4, "baz"},
+      {5, "bar"},
+      {7, "jazz"},
+      {9, "huzz"},
+    };
+
     for (int i = 1; i <= n; i++)
     {
         string res = "";
 
-        if (i % 3 == 0) res += "foo";
+        foreach( KeyValuePair<int, string> div in divisions )
+        {
+          if( i % div.Key == 0 )
+          {
+            res += div.Value;
+          }
+        }
 
-        if (i % 5 == 0) res += "bar";
-
-        if (i % 7 == 0) res += "jazz";
-
-        if (i % 3 != 0 && i % 5 != 0 && i % 7 != 0) res += i.ToString();
+        if (string.IsNullOrEmpty(res)) res += i.ToString();
 
         output.Add(res);
     }
